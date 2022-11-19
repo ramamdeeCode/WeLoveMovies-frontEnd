b8d1bb31c5636050ff58291dae50d35678698d7a
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-19 15:49:52 -0500

    changed api url

--- weLoveMovies-FrontEnd/src/utils/api.js
+++ weLoveMovies-FrontEnd/src/utils/api.js
@@ -1,5 +1,6 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://welovemovies-backend-q60c.onrender.com"; //"http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
